cec64712fcfe5b083b9f8572a22cdf2a238abd5a
arivera <andresrfelip@hotmail.com>
2022-03-16 20:40:33 -0500

    configuracion css

---
+++ components/layouts/Header.js
@@ -0,0 +1,60 @@
+import Buscar from "../ui/Buscar"
+import Navegacion from "./Navegacion";
+import Link from 'next/link';
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+const ContenedorHeader = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px){
+    display:flex;
+    justify-content: space-between;
+  }
+`;
+
+const Logo = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: 'Roboto Slab', serif;
+  margin-right: 2rem;
+`
+
+const Header = () => {
+  return (
+    <header
+      css={css`
+        border-bottom: 2px solid var(--gris3);
+        padding: 1rem 0;
+      `}
+    >
+        <ContenedorHeader>
+            <div>
+              <Link href='/'>
+                <Logo>P</Logo>
+              </Link>
+
+                <Buscar />
+
+                <Navegacion />
+            </div>
+
+            <div>
+               <p>Hola: Andres</p>
+
+               <button
+                type="button"
+               >Cerrar Sesión</button>
+
+               <Link href='/'>Login</Link>
+               <Link href='/'>Crear Cuenta</Link>
+            </div>
+        </ContenedorHeader>
+    </header>
+  )
+}
+
+export default Header
